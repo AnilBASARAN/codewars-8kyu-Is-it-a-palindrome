8570955bc95d977a2ac5383c8ac32fa61cd1dc11
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2022-05-13 00:43:41 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let isPalindrome=(x)=> x.toLowerCase().split('').reverse().join('') == x.toLowerCase() ? true : false 
+
+/*
+
+
+8kyu-Is it a palindrome?
+
+Write a function that checks if a given string (case insensitive) is a palindrome.
+
+https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/javascript
